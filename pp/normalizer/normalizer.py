5fac11144195d6a95d2082cd7e656d18b89e616d
meliksahturker <turkermeliksah@hotmail.com>
2022-02-24 19:47:02 +0300

    Typo correction now uses hunspell algorithm

--- pp/normalizer/normalizer.py
+++ pp/normalizer/normalizer.py
@@ -1,46 +1,37 @@
-#!/usr/bin/env python
-# coding: utf-8
 from typing import List
 from pathlib import Path
 import string
 
-import numpy as np
-
-from Levenshtein import distance as levenshtein_distance
+from hunspell import Hunspell
 
 from ._deasciifier import Deasciifier
 from ..stemmer_morph_analyzer import StemmerAnalyzer
 
-PATH = "../_resources/"
-PATH = str(Path(__file__).parent / PATH)
+RESOURCES_PATH = "../_resources/"
+RESOURCES_PATH = str(Path(__file__).parent / RESOURCES_PATH)
 
 class Normalizer():
     def __init__(self):
 
         # Multiword Lexicon
-        with open(PATH +'/mwe_lexicon.txt', 'r', encoding = 'utf-8') as f:
+        with open(RESOURCES_PATH +'/mwe_lexicon.txt', 'r', encoding = 'utf-8') as f:
             mwe = [line.strip() for line in f]
         dict_mwe = dict.fromkeys(mwe)
 
         # Word Lexicon merged from TDK-Zemberek, Zargan, Bilkent Creative Writing, Turkish Broadcast News
-        with open(PATH +'/turkish_known_words_lexicon.txt', 'r', encoding = 'utf-8') as f:
+        with open(RESOURCES_PATH +'/turkish_known_words_lexicon.txt', 'r', encoding = 'utf-8') as f:
             words_lexicon = [line.strip() for line in f]
         dict_words_lexicon = dict.fromkeys(words_lexicon)
 
-         # General Purpose Typo, Abbreviation, Social Media, etc. Normalization Lexicon
-        with open(PATH +'/typo_correction_lexicon.txt', 'r', encoding = 'utf-8') as f:
-            typo = [line.strip().split(',') for line in f]
-        dict_typo = dict(typo)
-
         self._mwe_lexicon = dict_mwe
         self._words_lexicon = dict_words_lexicon
-        self._typo_lexicon = dict_typo
         
         self._vowels = set("aeiou")
         self._consonants = set(string.ascii_lowercase) - self._vowels
         self._non_turkish_accent_marks = {'â':'a', 'ô':'o', 'î':'ı', 'ê':'e', 'û':'u'}
 
         self._stemmer_analyzer = StemmerAnalyzer()
+        self._hunspell = Hunspell('tr_TR', hunspell_data_dir= RESOURCES_PATH + '/tdd-hunspell-tr-1.1.0')
 
     @staticmethod
     def lower_case(text: str) -> str:
@@ -84,7 +75,7 @@ class Normalizer():
 
         'merhaba'
         """
-        return ''.join([t for t in text if t.isalnum()])
+        return ''.join([t for t in text if (t.isalnum() or t == " ")])
     
     def convert_number_to_word(self, tokens: List[str], num_dec_digits: int = 6)-> List[str]:
         """
@@ -147,11 +138,9 @@ class Normalizer():
         """
         return ''.join(self._non_turkish_accent_marks.get(char, char) for char in text)
     
-    def correct_typos(self, tokens: List[str], use_consonants: bool = True) -> List[str]:
+    def correct_typos(self, tokens: List[str]) -> List[str]:
         """
         Corrects spelling mistakes and typos.
-        Args:
-        use_levenshtein(bool): Whether to use levenshtein distance measure to find the correct word
 
         Input:
         tokens(List[str]): list of tokens
@@ -167,12 +156,16 @@ class Normalizer():
         """
         corrected_tokens = []
         for token in tokens:
-            if (self._is_token_valid_turkish(token)) or (token in self._words_lexicon):
+            if (self._is_token_valid_turkish(token)) or (self._hunspell.spell(token)):
                 corrected_tokens.append(token)
-            elif token in self._typo_lexicon:
-                corrected_tokens.append(self._typo_lexicon[token])
             else:
-                corrected_tokens.append(self._return_most_similar_word(token, use_consonants))
+                hunspell_suggestions = self._hunspell.suggest(token)
+                if len(hunspell_suggestions) > 0:
+                    corrected_token = hunspell_suggestions[0]
+                    corrected_tokens.append(corrected_token)
+                else:
+                    # there is no suggestion so return the original token
+                    corrected_tokens.append(token)
         
         return corrected_tokens
 
@@ -197,34 +190,6 @@ class Normalizer():
             deasciifier = Deasciifier(token)
             deasciified_tokens.append(deasciifier.convert_to_turkish())
         return deasciified_tokens
-    
-
-    def _return_most_similar_word(self, s1, use_consonants):
-        s1_consonant = "".join([l for l in s1 if l in self._consonants])
-        distance_list = []
-        consonant_distance_list = []
-        s2_list = []    # to keep track of known words when measuring similarity because dict is unordered
-
-        for s2 in self._words_lexicon:
-            s2_list.append(s2)
-
-            dist = levenshtein_distance(s1, s2)
-            distance_list.append(dist)
-
-            if use_consonants:
-                s2_consonant = "".join([l for l in s2 if l in self._consonants])
-                consonant_dist = levenshtein_distance(s1_consonant, s2_consonant)
-                consonant_distance_list.append(consonant_dist)
-
-        # This can be modified to idxmin of distance only to speedup a bit
-        # But this sort part takes 1% time of this function
-        if use_consonants:
-            table_distances = np.array([s2_list, distance_list, consonant_distance_list]).T
-            sorted_table_distances = table_distances[np.lexsort((table_distances[:, 2].astype(int), table_distances[:, 1].astype(int)))]
-            most_similar_word = sorted_table_distances[0, 0]
-        else:
-            most_similar_word = s2_list[np.argmin(distance_list)]
-        return most_similar_word
 
     def _is_token_valid_turkish(self, token):
         valid_according_to_stemmer_analyzer = not (self._stemmer_analyzer.candidate_generator.get_analysis_candidates(token)[0][-1] == 'Unknown')
