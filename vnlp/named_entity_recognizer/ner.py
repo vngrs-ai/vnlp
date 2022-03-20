c8723d2d37631dbe73155106f12386cd36648999
meliksahturker <turkermeliksah@hotmail.com>
2022-03-20 18:42:33 +0300

    bugfix with ner displacy format where matching substring caused collisions.

--- vnlp/named_entity_recognizer/ner.py
+++ vnlp/named_entity_recognizer/ner.py
@@ -167,17 +167,28 @@ class NamedEntityRecognizer:
             token_loc = {}
             for duo in ner_result:
                 word = duo[0]
-                
+
                 # https://stackoverflow.com/a/13989661/4505301
                 if word == '.':
                     continue
-                    
+
                 if not word in token_loc:
                     token_loc[word] = []
                 for match in re.finditer(word, text):
                     start, end = match.start(), match.end()
-                    
-                    if not (start, end) in token_loc[word]:
+                    substring_of_prev_strings = False
+                    # check if this match is part another previously matched string and skip it if so
+                    for token in token_loc:
+                        list_of_token_indices = token_loc[token]
+                        for prev_token_indices in list_of_token_indices:
+                            prev_token_start = prev_token_indices[0]
+                            prev_token_end = prev_token_indices[1]
+                            
+                            if (start >= prev_token_start) and (end <= prev_token_end):
+                                substring_of_prev_strings = True
+                                break
+
+                    if (not (start, end) in token_loc[word]) and (not substring_of_prev_strings):
                         token_loc[word].append((start, end))
 
             # Process for Spacy
