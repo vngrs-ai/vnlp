5294415c35f127a99f9492e098b9425f9458de1f
meliksahturker <turkermeliksah@hotmail.com>
2022-03-30 13:17:56 +0300

    init improvements and checkpoint commit

--- vnlp/__init__.py
+++ vnlp/__init__.py
@@ -1,11 +1,11 @@
 from .dependency_parser import DependencyParser
 from .named_entity_recognizer import NamedEntityRecognizer
-from .normalizer.normalizer import Normalizer
-from .part_of_speech_tagger.pos_tagger import PoSTagger
-from .sentence_splitter.sentence_splitter import RuleBasedSentenceSplitter as SentenceSplitter
-from .sentiment_analyzer.sentiment_analyzer import SentimentAnalyzer
-from .stemmer_morph_analyzer.stemmer_morph_analyzer import  StemmerAnalyzer
-from .stopword_remover.stopword_remover import StopwordRemover
+from .normalizer import Normalizer
+from .part_of_speech_tagger import PoSTagger
+from .sentence_splitter import SentenceSplitter
+from .sentiment_analyzer import SentimentAnalyzer
+from .stemmer_morph_analyzer import StemmerAnalyzer
+from .stopword_remover import StopwordRemover
 
 __all__ = ['DependencyParser', 'NamedEntityRecognizer', 'Normalizer', 
            'PoSTagger', 'SentenceSplitter', 'SentimentAnalyzer', 
--- vnlp/sentiment_analyzer/sentiment_analyzer.py
+++ vnlp/sentiment_analyzer/sentiment_analyzer.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import pickle
 
 import numpy as np
