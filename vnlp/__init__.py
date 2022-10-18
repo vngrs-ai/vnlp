6bc5341ec14251a1b6e95f4d851e0cc51baec4eb
meliksahturker <turkermeliksah@hotmail.com>
2022-10-18 18:11:03 +0300

    tf gpu memory growth update. prevents tf from allocating whole memory. otherwise might result in OOM error in some cases

--- vnlp/__init__.py
+++ vnlp/__init__.py
@@ -1,3 +1,9 @@
+import tensorflow as tf
+# Prevent tensorflow from allocating whole GPU memory.
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
+
 from .dependency_parser import DependencyParser
 from .named_entity_recognizer import NamedEntityRecognizer
 from .normalizer import Normalizer
