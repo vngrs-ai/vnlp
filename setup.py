08f2f28c70fc508b2f6322d316652146077d572f
meliksahturker <turkermeliksah@hotmail.com>
2024-11-27 15:31:29 +0300

    tensorflow dependency issue is resolved.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="vngrs-nlp",
-    version="0.2.3",
+    version="0.2.4",
     description="Turkish NLP Tools developed by VNGRS.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
     ],
     project_urls={
         "Homepage": "https://github.com/vngrs-ai/vnlp",
@@ -40,17 +41,12 @@ setup(
     },
     packages=find_packages(exclude=["turkish_embeddings"]),
     include_package_data=True,
-    setup_requires=[
-        "swig==3.0.12",
-    ],
     install_requires=[
         'tensorflow<2.6.0; python_version < "3.8"',
-        'tensorflow>=2.6.0; python_version >= "3.8"',
-        "swig==3.0.12",
+        'tensorflow>=2.6,<2.16; python_version >= "3.8"',
         "regex",
         "requests",
         "sentencepiece",
-        "jamspell"
     ],
     extras_require={"extras": ["gensim", "spacy"], "develop": ["pre-commit", "pytest"]},
     entry_points={"console_scripts": ["vnlp=vnlp.bin.vnlp:main"]},
